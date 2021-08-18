268412e5ee94a8e8b0898ea41da3a9133492372d
pedroguaillas <peter.tufi@gmail.com>
2021-08-18 16:14:37 -0500

    stdClass to array obj

--- app/Http/Controllers/WSSriController.php
+++ app/Http/Controllers/WSSriController.php
@@ -50,7 +50,7 @@ class WSSriController
                 case VoucherStates::RETURNED:
                     $mensajes = $result->RespuestaRecepcionComprobante->comprobantes->comprobante->mensajes->mensaje;
 
-                    $mensajes = json_decode(json_encode($mensajes));
+                    $mensajes = json_decode(json_encode($mensajes), true);
 
                     var_dump($mensajes);
 
