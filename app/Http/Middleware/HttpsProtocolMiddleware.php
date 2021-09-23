3a57ced8369020e2dec0d883ec8b9f0171a30ec8
pedroguaillas <peter.tufi@gmail.com>
2021-09-23 05:06:32 -0500

    Probando Middleware

--- app/Http/Middleware/HttpsProtocolMiddleware.php
+++ app/Http/Middleware/HttpsProtocolMiddleware.php
@@ -8,6 +8,7 @@ class HttpsProtocolMiddleware
 {
     public function handle($request, Closure $next)
     {
+        var_dump('Probando Middleware');
         if (!$request->secure() && app()->environment('production')) {
             return redirect()->secure($request->getRequestUri());
         }
